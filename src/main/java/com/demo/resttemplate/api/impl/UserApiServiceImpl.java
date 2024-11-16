54b7086cbce76a0085ee4e7986d24a78d7a351c3
noobdeveloper03 <noob.developer03@gmail.com>
2024-11-16 15:27:40 +0800

    Changed image to 1.0.7

--- src/main/java/com/demo/resttemplate/api/impl/UserApiServiceImpl.java
+++ src/main/java/com/demo/resttemplate/api/impl/UserApiServiceImpl.java
@@ -3,6 +3,8 @@ package com.demo.resttemplate.api.impl;
 import com.demo.resttemplate.api.dto.UserApi;
 import com.demo.resttemplate.api.UserApiService;
 import com.demo.resttemplate.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
@@ -18,14 +20,15 @@ public class UserApiServiceImpl implements UserApiService {
     @Autowired
     private RestTemplate restTemplate;
 
+    private Logger logger = LoggerFactory.getLogger(UserApiServiceImpl.class);
+
     @Autowired
     private Environment env;
 
     @Override
     public List<User> getUsers() {
-        System.out.println("Rest API Endpoint = " + env.getProperty("api.user.endpoint"));
+        logger.info("Rest API Endpoint = {}",env.getProperty("api.user.endpoint"));
         UserApi userApi = restTemplate.getForObject(env.getProperty("api.user.endpoint"), UserApi.class);
-        System.out.println("Fetch Data size = " + userApi.getUsers().size());
         return userApi.getUsers();
     }
 }
